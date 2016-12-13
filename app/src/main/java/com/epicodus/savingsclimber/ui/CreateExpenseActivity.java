d6a8a2ee1fb84c9d883569a5b4e955e49bb8a9b5
Andrew Merrell and Caleb Stevenson <andrew@>
2016-12-13 13:25:03 -0800

    add createExpenseActivity

---
+++ app/src/main/java/com/epicodus/savingsclimber/ui/CreateExpenseActivity.java
@@ -0,0 +1,13 @@
+package com.epicodus.savingsclimber.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_expense);
+    }
+}
